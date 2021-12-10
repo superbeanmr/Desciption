c603a5eddf94c17b1822e115f4f889a279260504
superbeanmr <beanmr888@gmail.com>
2021-12-10 11:29:09 +0800

    prod commit

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x20f99E8c2583322fa3Dc3f5b00215094D8A45C3f'
+
+export const INIT_CODE_HASH = '7d9225fa97f1d72aefc85b102545d08ff9faef6b3a6583fddae791130b56f047'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
